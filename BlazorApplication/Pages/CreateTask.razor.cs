a0d0568756eac09ccf94ad990758a81994345f0c
Roman Zabela <zaberoman@yandex.ru>
2023-01-12 01:00:43 +0200

    added Authorized attributes for checking

--- BlazorApplication/Pages/CreateTask.razor.cs
+++ BlazorApplication/Pages/CreateTask.razor.cs
@@ -11,6 +11,7 @@ namespace BlazorApplication.Pages
 		private string _timeFrameMode = "1";
 		private Enums.TaskCategory _taskCategory = Enums.TaskCategory.Easy;
 		private SuccessNotification _notification;
+		private string _time { get; set; }
 
 
         [Inject]
@@ -18,30 +19,85 @@ namespace BlazorApplication.Pages
 
 		private async void Create()
 		{
-			//_task.CreateDate = DateTime.Now;	
-			//_task.UpdateDate = DateTime.Now;
-			//_task.CreateUserId = 1;
-			//_task.UpdateUserId = 1;
-			//_task.StatusId = 1;
+
 			_task.TaskCategoryId = (int)_taskCategory;
 
-			switch (_timeFrameMode)
-			{
-				case "1":
-					_task.Timeframe = new TimeSpan(0, 30, 0);
-					break;
-				case "2":
-					_task.Timeframe = new TimeSpan(1, 0, 0);
-					break;
-				case "3":
-					_task.Timeframe = new TimeSpan(1, 30, 0);
-					break;
-				case "4":
-					_task.Timeframe = new TimeSpan(2, 0, 0);
-					break;
-			}
-			
-			await TaskRepo.CreateTask(_task);
+            //switch (_timeFrameMode)
+            //{
+            //	case "1":
+            //		_task.Timeframe = new TimeSpan(0, 30, 0);
+            //		break;
+            //	case "2":
+            //		_task.Timeframe = new TimeSpan(1, 0, 0);
+            //		break;
+            //	case "3":
+            //		_task.Timeframe = new TimeSpan(1, 30, 0);
+            //		break;
+            //	case "4":
+            //		_task.Timeframe = new TimeSpan(2, 0, 0);
+            //		break;
+            //}
+
+            if (_time.Contains('.'))
+            {
+                _time = _time.Replace('.', ':');
+            }
+            if (_time.Contains(':'))
+            {
+                string[] numbers = _time.Split(':');
+                if (numbers.Length == 3)
+                {
+
+                    try
+                    {
+                        if (Convert.ToInt32(numbers[0]) < 25)
+                        {
+                            _task.Timeframe = new TimeSpan(Convert.ToInt32(numbers[0]), Convert.ToInt32(numbers[1]), Convert.ToInt32(numbers[2]));
+                        }
+                        else
+                        {
+                            return;
+                        }
+
+                    }
+                    catch
+                    {
+                        return;
+                    }
+
+                }
+                else if (numbers.Length == 2)
+                {
+                    try
+                    {
+                        if (Convert.ToInt32(numbers[0]) < 25)
+                        {
+                            _task.Timeframe = new TimeSpan(Convert.ToInt32(numbers[0]), Convert.ToInt32(numbers[1]), 0);
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                    catch
+                    {
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    _task.Timeframe = new TimeSpan(0, Convert.ToInt32(_time), 0);
+                }
+                catch
+                {
+                    return;
+                }
+            }
+
+            await TaskRepo.CreateTask(_task);
 			_notification.Show();
 		}
 	}
