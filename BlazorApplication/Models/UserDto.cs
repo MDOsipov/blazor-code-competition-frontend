d951fadfe739e06b60fc089862389f950798bb6d
MDOsipov <dos12345671@gmail.com>
2023-01-18 22:24:00 +0200

    CCMS-35 - added users management

---
+++ BlazorApplication/Models/UserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApplication.Models
+{
+	public class UserDto
+	{
+        public string Id { get; set; }
+        public string Email { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public bool Blocked { get; set; }
+		public string RoleName { get; set; }
+        public string RoleId { get; set; }
+
+    }
+}
