03378b7ca5b035288fb2ac572443faeb5b861199
MDOsipov <dos12345671@gmail.com>
2023-02-02 23:32:05 +0200

    CCMS-50 - enriched participant dashboard

--- BlazorApplication/Interfaces/ITaskToTeamHttpRepository.cs
+++ BlazorApplication/Interfaces/ITaskToTeamHttpRepository.cs
@@ -5,6 +5,7 @@ namespace BlazorApplication.Interfaces
 	public interface ITaskToTeamHttpRepository
 	{
 		Task<TaskToTeam> GetTaskToTeamByTaskIdAndTeamIdAndCompetitionId(string taskId, string teamId, string competitionId);
+		Task<int> GetOverallScoreByTeamIdAndCompetitionId(string teamId, string competitionId);
 		System.Threading.Tasks.Task AddTaskToTeam(TaskToTeam taskToTeam);
 		System.Threading.Tasks.Task DeleteTeamTaskByTaskIdAndTeamId(string taskId, string teamId);
 		System.Threading.Tasks.Task SubmitTask(string taskId, string teamId, SubmitTaskDataDto submitTaskDataDto);
